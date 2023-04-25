bd10b7e18a11dc1fd16c918387c42382e8e4622e
Setare <zahrasadat.azarmsa@dci.education>
2023-04-25 13:29:46 +0200

    Add statrus and numOfplayedGames to userSchema

--- Server/server.js
+++ Server/server.js
@@ -5,15 +5,12 @@ import cors from "cors"
 dotenv.config();
 const app = express();
 app.use(express.json())
-app.use(cors({origin:"http://localhost:5173", exposedHeaders:["token"]}))
+app.use(cors({ origin: "http://localhost:5173", exposedHeaders: ["token"] }))
 mongoose
     .connect(
-        process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true }
-        
-
-        ,
+        process.env.URI, { useNewUrlParser: true, useUnifiedTopology: true },
         { useNewUrlParser: true, useUnifiedTopology: true }
     )
     .then(() => console.log('Database connected! '))
     .catch((err) => console.log('Database is not connected! ', err.message));
-    app.listen(8000, () => console.log(`The server is listening on port ${process.env.PORT}`));
+app.listen(8000, () => console.log(`The server is listening on port ${process.env.PORT}`));
