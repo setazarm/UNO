69aa463a7efccd33988124b8645805675d4f7d33
Setare <zahrasadat.azarmsa@dci.education>
2023-04-25 10:18:40 +0200

    initialize user model

---
+++ Server/models/userSchema.js
@@ -0,0 +1,37 @@
+import { Schema, model } from "mongoose"
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+
+    Avatar: {
+        type: String,
+        default: function () {
+            return `https://ui-avatars.com/api/?name=${this.name}&background=random&color=fff&rounded=true&size=50`
+        }
+    },
+    points:{
+        type:Number,
+        default:0
+    },
+    role: {
+        type: String,
+        default: "user",
+        enum: ["user", "admin"]
+    },
+}, { timestamps: true });
+
+const User = model('User', userSchema);
+
+export default User
