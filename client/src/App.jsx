60f478beb3f391c473b5d5a8a1c0364ccf242a0c
Setare <setazarm@gmail.com>
2023-05-02 14:27:24 +0200

    add navigation to createRoom component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import LoginForm from "./Components/LoginForm.jsx";
 import NavBar from "./Components/NavBar.jsx";
 import Lobby from "./Components/Lobby.jsx";
 import CreateRoom from "./Components/CreateRoom.jsx";
+import GameRoom from "./Components/GameRoom";
 function App() {
     return (
         <>
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/register" element={<RegisterForm />} />
                 <Route path="/lobby" element={<Lobby />} />
                 <Route path="/createRoom" element={<CreateRoom/>} />
+                <Route path="/game" element={<GameRoom/>} />
             </Routes>
         </>
     );
--- client/src/Components/CreateRoom.jsx
+++ client/src/Components/CreateRoom.jsx
@@ -1,10 +1,11 @@
 import {socket} from  "../socket"
-
+import { useNavigate } from "react-router-dom"
 const CreateRoom = () => {
-    
+    const navigate = useNavigate()
     const createRoom = (e) => {
         e.preventDefault()
         socket.emit('createRoom', {roomName: e.target.room.value, password: e.target.password.value})
+        navigate('/game')
         }
   return (
     <form onSubmit={createRoom}>
