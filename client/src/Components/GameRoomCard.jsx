dfd5224002a50392b8df40572ae7a6117d60ebf7
Marvin Deutz <marvin.deutz@yahoo.com>
2023-05-02 16:26:18 +0200

    Finalize lobby displaying all rooms

---
+++ client/src/Components/GameRoomCard.jsx
@@ -0,0 +1,11 @@
+const GameRoomCard = ({ room }) => {
+    return (
+        <div className="border-gray-700 rounded-sm border-2 mb-1 w-32">
+            <h2>{room.roomName}</h2>
+            <h3>{room.players.length} / 4 Players</h3>
+            <h3>Password needed: {room.password ? "Yes" : "No"}</h3>
+            <button className="border-2 border-black rounded-md p-1">Join</button>
+        </div>
+    );
+};
+export default GameRoomCard;
