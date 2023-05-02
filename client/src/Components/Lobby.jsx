1e751080df3de03745d286e771ee8b052407361f
Marvin Deutz <marvin.deutz@yahoo.com>
2023-05-02 16:23:19 +0200

    Add lobby logic

--- client/src/Components/Lobby.jsx
+++ client/src/Components/Lobby.jsx
@@ -1,15 +1,35 @@
-import { useNavigate } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import GameRoomCard from "./GameRoomCard.jsx";
 
 const Lobby = () => {
-    const navigate = useNavigate()
-  return (
-    <div>Lobby
-        <button onClick={()=>{
-            navigate('/createRoom')
+    const navigate = useNavigate();
+    const [rooms, setRooms] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:8000/rooms").then((res) => {
+            console.log(res.data.data);
+            setRooms(res.data.data);
+        });
+    }, []);
 
-        }}>Create a new Room</button>
-    </div>
-  )
-}
+    return (
+        <div>
+            <div className="flex gap-4">
+                {rooms.map((room) => {
+                    return <GameRoomCard room={room} key={room._id} />;
+                })}
+            </div>
+            <button
+                onClick={() => {
+                    navigate("/createRoom");
+                }}
+                className="border-2 border-black rounded p-1"
+            >
+                Create a new Room
+            </button>
+        </div>
+    );
+};
 
-export default Lobby
+export default Lobby;
