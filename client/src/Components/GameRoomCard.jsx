dc3208df187d95d9008ef36fbbd7dc2c3b175560
Marvin Deutz <marvin.deutz@yahoo.com>
2023-05-08 15:23:20 +0200

    Fix password checking for rooms

--- client/src/Components/GameRoomCard.jsx
+++ client/src/Components/GameRoomCard.jsx
@@ -1,15 +1,29 @@
+import { useEffect } from "react";
 import { socket } from "../socket.js";
 import { useNavigate } from "react-router-dom";
 
-const GameRoomCard = ({ room, setShow }) => {
+const GameRoomCard = ({ room, setShow, setPassword, passwordCorrect }) => {
     const user = JSON.parse(localStorage.getItem("user"));
     const navigate = useNavigate();
     const joinRoom = () => {
-        socket.connect();
-        // room.password ? setShow(true) : null
-        navigate(`/game/${room._id}`);
+        if (!room.password) {
+            socket.connect();
+            navigate(`/game/${room._id}`);
+        } else {
+            setShow(true);
+            setPassword(room.password);
+        }
     };
+
+    useEffect(() => {
+        if (passwordCorrect) {
+            socket.connect();
+            navigate(`/game/${room._id}`);
+        }
+    }, [passwordCorrect]);
+
     const disabled = room.players.length >= 4;
+
     return (
         <div className="border-gray-700 rounded-sm border-2 mb-1 w-32">
             <h2>{room.roomName}</h2>
--- client/src/Components/RoomPasswordModal.jsx
+++ client/src/Components/RoomPasswordModal.jsx
@@ -1,13 +1,28 @@
-const RoomPasswordModal = ({ show, setShow }) => {
+const RoomPasswordModal = ({ show, setShow, setPasswordCorrect, password }) => {
     if (!show) {
         return null;
     }
+    const handlePasswordSubmit = (e) => {
+        e.preventDefault();
+        if (e.target.roomPassword.value === password) {
+            setPasswordCorrect(true);
+        } else {
+            alert("wrong password");
+        }
+    };
     return (
         <div>
             <button onClick={() => setShow(false)}>X</button>
             <h2>Please enter the room password</h2>
-            <input type="text" name="roomPassword" id="roomPassword" placeholder="password..." />
-            <button>Join</button>
+            <form onSubmit={handlePasswordSubmit}>
+                <input
+                    type="text"
+                    name="roomPassword"
+                    id="roomPassword"
+                    placeholder="password..."
+                />
+                <button>Join</button>
+            </form>
         </div>
     );
 };
